e18027b0e5260ace4148da360e1b3b99d43fd74e
Tarek Allam <t.allam.jr@gmail.com>
2020-12-01 15:09:15 +0000

    [AOC20][RUST] Add solution for day 1
    Although same algorithm in Python solution, this rust version has been
    heavily influenced by Oscar Giles solution.
    
    Refs:
        - https://github.com/alan-turing-institute/advent-of-code-2020/tree/master/day-01/rust_Oscar
    
    	modified:   Cargo.toml
    	modified:   src/main.rs

--- 2020/day-01/rust/src/main.rs
+++ 2020/day-01/rust/src/main.rs
@@ -1,3 +1,44 @@
+use itertools::Itertools;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let items = read_input("../data/input.txt");
+    let ans1 = day1(&items, 2);
+
+    println!("Day 1 Part 1 = {}", ans1);
+    let ans2 = day1(&items, 3);
+    println!("Day 1 Part 2 = {}", ans2);
+}
+
+fn read_input(file_name: &str) -> Vec<i32> {
+    let file_contents =
+        fs::read_to_string(file_name).expect("Something went wrong reading the file");
+
+    let lines: Vec<i32> = file_contents.lines().map(|x| x.parse().unwrap()).collect();
+
+    return lines;
+}
+
+fn day1(items: &Vec<i32>, k: usize) -> i32 {
+    let zip_items = items.iter().combinations(k);
+    let sums: Vec<_> = zip_items
+        .filter(|x| x.iter().map(|y| *y).sum::<i32>() == 2020)
+        .collect();
+    assert_eq!(sums.len(), 1, "Returned more than one value");
+    let part1_ans = sums[0].iter().map(|x| *x).product();
+    return part1_ans;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_day1() {
+        let items = read_input("../data/input.txt");
+        let answer1 = day1(&items, 2);
+        assert_eq!(answer1, 703131);
+        let answer2 = day1(&items, 3);
+        assert_eq!(answer2, 272423970);
+    }
 }
